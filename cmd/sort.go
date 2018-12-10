3b1c0dd9b1b46e0c31f95ad3a1a50b6a6fbc30f5
Jorge Henriquez <joahenri@ucsc.edu>
2018-12-09 19:34:20 -0800

    update cli tool info

--- cmd/sort.go
+++ cmd/sort.go
@@ -28,13 +28,7 @@ import (
 // sortCmd represents the sort command
 var sortCmd = &cobra.Command{
 	Use:   "sort",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Print the sorted values in a file",
 	Run: func(cmd *cobra.Command, args []string) {
 		// Get the numbers to sort
 		inputFileString, err := cmd.Flags().GetString("file")
